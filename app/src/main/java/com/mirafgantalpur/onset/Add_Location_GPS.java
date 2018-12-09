0d61c2bab8debac42dde7ee5a766f3176da80b69
mirafgantalpur <mir.talpur@uoit.net>
2018-12-09 13:22:10 -0500

    Fixed small problems with MediaPlayer Activity, added other activities to help plan out flow of application, added method to convert YouTube links inputted by user to YouTube Video IDs for YouTube API.

--- app/src/main/java/com/mirafgantalpur/onset/Add_Location_GPS.java
+++ app/src/main/java/com/mirafgantalpur/onset/Add_Location_GPS.java
@@ -3,11 +3,11 @@ package com.mirafgantalpur.onset;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Add_Location_GPS extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add__location__gps);
     }
 }
---
+++ app/src/main/java/com/mirafgantalpur/onset/LocationInfo.java
@@ -0,0 +1,68 @@
+package com.mirafgantalpur.onset;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LocationInfo extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_info);
+
+        youTubeView = findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            String youTubeLink = getYouTubeId("https://www.youtube.com/watch?v=4CbLXeGSDxg");
+            player.cueVideo(youTubeLink);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+        }
+    }
+
+    protected Provider getYouTubePlayerProvider() {
+        return youTubeView;
+    }
+
+    public static String getYouTubeId (String url) {
+        String pattern = "(?<=youtu.be/|watch\\?v=|/videos/|embed\\/)[^#\\&\\?]*";
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(url);
+        if(matcher.find()){
+            return matcher.group();
+        } else {
+            return "error";
+        }
+    }
+}
