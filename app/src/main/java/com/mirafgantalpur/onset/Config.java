0fdd4930fd0b9623a18ddb7a317223e4fd0a4577
mirafgantalpur <mir.talpur@uoit.net>
2018-12-09 11:21:25 -0500

    Replaced MediaPlayer with YouTube Data API for better functionality. Really early UI design right now, just to show general picture of the location page.

---
+++ app/src/main/java/com/mirafgantalpur/onset/Config.java
@@ -0,0 +1,9 @@
+package com.mirafgantalpur.onset;
+
+public class Config {
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCrcOeSH9Qe6BUgHzchlCer0o2vQnG4sPs ";
+
+}
