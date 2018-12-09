dc366f65ad751ecc4969d76c094c3c7255d9f944
mirafgantalpur <mir.talpur@uoit.net>
2018-12-09 18:25:37 -0500

    Changed sign up page to have scroll view layout, making it easier to check inputted answers when keyboard is up.

--- app/src/main/java/com/mirafgantalpur/onset/SignUp.java
+++ app/src/main/java/com/mirafgantalpur/onset/SignUp.java
@@ -1,7 +1,9 @@
 package com.mirafgantalpur.onset;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+    public void onSignUp(View view)
+    {
+        Intent intent = new Intent(SignUp.this, LocationInfo.class);
+        startActivity(intent);
+    }
+
+    public void onBack(View view)
+    {
+        Intent intent = new Intent(SignUp.this, MainActivity.class);
+        startActivity(intent);
     }
 }
