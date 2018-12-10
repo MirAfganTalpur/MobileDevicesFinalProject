6ccb0fbc9b99c6aec346c01b9c21b85b8e8d4d35
mirafgantalpur <mir.talpur@uoit.net>
2018-12-10 13:37:12 -0500

    Password and field checks on sign up page.

--- app/src/main/java/com/mirafgantalpur/onset/SignUp.java
+++ app/src/main/java/com/mirafgantalpur/onset/SignUp.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
 
@@ -14,13 +16,50 @@ public class SignUp extends AppCompatActivity {
     }
     public void onSignUp(View view)
     {
-        Intent intent = new Intent(SignUp.this, LocationInfo.class);
-        startActivity(intent);
+        EditText [] fields = new EditText[5];
+
+        EditText full_name = findViewById(R.id.full_name);
+        EditText username = findViewById(R.id.username);
+        EditText email = findViewById(R.id.email);
+        EditText password1 = findViewById(R.id.password);
+        EditText password2 = findViewById(R.id.password_check);
+        TextView error = findViewById(R.id.error);
+
+        fields[0] = full_name;
+        fields[1] = username;
+        fields[2] = email;
+        fields[3] = password1;
+        fields[4] = password2;
+
+        String password_entry = password1.getText().toString();
+        String password_check = password2.getText().toString();
+
+        if (isEmpty(fields)) {
+            error.setText("Please fill out all fields.");
+        } else {
+            if (password_check!=password_entry) {
+                error.setText("Passwords do not match, please try again.");
+                password1.getText().clear();
+                password2.getText().clear();
+            } else {
+                Intent intent = new Intent(SignUp.this, LocationInfo.class);
+                startActivity(intent);
+            }
+        }
     }
 
     public void onBack(View view)
     {
         Intent intent = new Intent(SignUp.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public boolean isEmpty (EditText[] fields){
+        for (int i = 0; i < fields.length; i++ ){
+            if (fields[i].getText().toString().length() == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
