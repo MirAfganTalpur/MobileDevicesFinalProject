ee33068d5ac9be3755aa0ee88eb90daf1492df52
jon-perry <jonathan.perry.17@gmail.com>
2018-12-11 12:17:57 -0500

    Some minor tweaks to make storage structure better

--- app/src/main/java/com/mirafgantalpur/onset/FirebaseHelper.java
+++ app/src/main/java/com/mirafgantalpur/onset/FirebaseHelper.java
@@ -1,18 +1,10 @@
 package com.mirafgantalpur.onset;
 
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,11 +13,12 @@ public final class FirebaseHelper {
     static void addLocation(String username, Location location) {
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
         Map<String, Object> locationInfo = new HashMap<>();
-        locationInfo.put("location", location);
-        ref.child("users").child(username).child("locations").child(location.getUuid().toString()).updateChildren(locationInfo);
+        locationInfo.put(location.getUuid().toString(), location);
+        ref.child("users").child(username).child("locations").updateChildren(locationInfo);
         if (!location.isOnlyForMe()) {
             location.setAuthorUsername(username);
-            ref.child("sharedLocations").child(location.getUuid().toString()).updateChildren(locationInfo);
+            ref.child("sharedLocations").updateChildren(locationInfo);
+            ref.child("users").child(username).child("sharedLocations").updateChildren(locationInfo);
         }
     }
 
@@ -33,14 +26,16 @@ public final class FirebaseHelper {
     static void updateLocation(String username, Location location) {
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
         Map<String, Object> locationInfo = new HashMap<>();
-        locationInfo.put("location", location);
-        ref.child("users").child(username).child("locations").child(location.getUuid().toString()).updateChildren(locationInfo);
+        locationInfo.put(location.getUuid().toString(), location);
+        ref.child("users").child(username).child("locations").updateChildren(locationInfo);
         if (location.isOnlyForMe()) {
             location.setAuthorUsername("");
             deleteLocationFromShared(location.getUuid().toString());
             ref.child("users").child(username).child("sharedLocations").child(location.getUuid().toString()).removeValue();
         } else {
-            ref.child("sharedLocations").child(location.getUuid().toString()).updateChildren(locationInfo);
+            location.setAuthorUsername(username);
+            ref.child("sharedLocations").updateChildren(locationInfo);
+            ref.child("users").child(username).child("sharedLocations").updateChildren(locationInfo);
         }
     }
 
@@ -53,5 +48,9 @@ public final class FirebaseHelper {
     static void deleteLocation(String username, String uuid) {
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
         ref.child("users").child(username).child("locations").child(uuid).removeValue();
+        ref.child("users").child(username).child("sharedLocations").child(uuid).removeValue();
+        ref.child("sharedLocations").child(uuid).removeValue();
     }
+
+
 }
--- app/src/main/java/com/mirafgantalpur/onset/MainActivity.java
+++ app/src/main/java/com/mirafgantalpur/onset/MainActivity.java
@@ -18,9 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FirebaseAuth.getInstance().signOut();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        Location location = new Location("namesssss", "type", "addssssssress", "city", "country",
-                "permission", "feauture", true, true, UUID.fromString("2bf07803-5d84-4d65-a931-a0482d46c824"));
-        FirebaseHelper.updateLocation("zxc",location);
+//        Location location = new Location("naaaaaaa", "type", "addssssssress", "city", "country",
+//                "permission", "feauture", true, false, UUID.fromString("2bf07803-5d84-4d65-a931-a0482d46c824"));
+        Location location = new Location("naaaaaaa", "type", "addssssssress", "city", "country",
+            "permission", "feauture", true, false, UUID.fromString("842c8a70-d9a3-49cf-bfa9-a6d5618cfdef"));
+//        FirebaseHelper.addLocation("testing",location);
+        FirebaseHelper.updateLocation("testing",location);
+//        FirebaseHelper.deleteLocation("testing", "5b1c517a-8d45-43b3-8639-aba468bdae89");
         if (user != null) {
             Log.e("test", "user is signed in");
             FirebaseAuth.getInstance().signOut();
