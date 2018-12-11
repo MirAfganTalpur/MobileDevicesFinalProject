724c4655f61a99d4700521dceb201f67754304f9
mirafgantalpur <mir.talpur@uoit.net>
2018-12-10 20:44:40 -0500

    Added functionality to add location activity, can now choose address based on location, moving the marker, or by clicking on the map.

--- app/src/main/java/com/mirafgantalpur/onset/AddLocation.java
+++ app/src/main/java/com/mirafgantalpur/onset/AddLocation.java
@@ -10,7 +10,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,25 +19,28 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-
 public class AddLocation extends FragmentActivity implements OnMapReadyCallback {
     LocationManager locationManager;
     Context mContext;
     private GoogleMap mMap;
     public  static final int PERMISSIONS_MULTIPLE_REQUEST = 123;
+    EditText locationAddress;
+    Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
         mContext = this;
-        FragmentManager fragManager = getSupportFragmentManager();
+        locationAddress = findViewById(R.id.location_address_editText);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
@@ -47,6 +50,16 @@ public class AddLocation extends FragmentActivity implements OnMapReadyCallback 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                 2000,
                 10, locationListenerGPS);
+
+        locationAddress.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    String address = locationAddress.getText().toString();
+                    toAddress(mContext,address);
+                }
+            }
+        });
     }
 
     @Override
@@ -57,7 +70,24 @@ public class AddLocation extends FragmentActivity implements OnMapReadyCallback 
         mMap.getUiSettings().setZoomGesturesEnabled(true);
     }
 
-    LocationListener locationListenerGPS=new LocationListener() {
+    public void toAddress (Context context, String inputtedAddress) {
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        try {
+            address = coder.getFromLocationName(inputtedAddress, 5);
+
+            Address location = address.get(0);
+            double latitude = location.getLatitude();
+            double longitude =location.getLongitude();
+
+            findLocation(latitude,longitude);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    LocationListener locationListenerGPS = new LocationListener() {
         @Override
         public void onLocationChanged(android.location.Location location) {
             double latitude=location.getLatitude();
@@ -68,24 +98,17 @@ public class AddLocation extends FragmentActivity implements OnMapReadyCallback 
         }
 
         @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
 
         @Override
-        public void onProviderEnabled(String provider) {
-
-        }
+        public void onProviderEnabled(String provider) {}
 
         @Override
-        public void onProviderDisabled(String provider) {
-
-        }
+        public void onProviderDisabled(String provider) {}
     };
 
     public void findLocation(double latitude, double longitude){
         LatLng newLocation = new LatLng(latitude, longitude);
-
 
         if (Geocoder.isPresent()) {
             Geocoder geocoder = new Geocoder(this,
@@ -94,6 +117,7 @@ public class AddLocation extends FragmentActivity implements OnMapReadyCallback 
                 List<Address> ls = geocoder.getFromLocation(latitude, longitude, 1);
                 for (Address addr: ls) {
                     String address = addr.getAddressLine(0);
+                    locationAddress.setText(address);
 
                     String city = addr.getLocality();
 
@@ -106,8 +130,39 @@ public class AddLocation extends FragmentActivity implements OnMapReadyCallback 
                     String phone = addr.getPhone();
 
                     String url = addr.getUrl();
+
                     mMap.clear();
-                    mMap.addMarker(new MarkerOptions().position(newLocation).title("Marker:"+address).snippet(city + ", " + prov + ", " + country + ", " + postalCode + ", " + phone + ", " + url));
+
+                    marker = mMap.addMarker(new MarkerOptions().position(newLocation).title("Marker:"+address).snippet(city + ", " + prov + ", " + country + ", " + postalCode + ", " + phone + ", " + url).draggable(true));
+
+                    mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                        @Override
+                        public void onMapClick(LatLng latLng) {
+                            marker.setPosition(latLng);
+                            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                            double newLat = latLng.latitude;
+                            double newLong = latLng.longitude;
+                            findLocation(newLat,newLong);
+                        }
+                    });
+
+                    mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+                        @Override
+                        public void onMarkerDragStart(Marker arg0) {}
+
+                        @SuppressWarnings("unchecked")
+                        @Override
+                        public void onMarkerDragEnd(Marker arg0) {
+                            mMap.animateCamera(CameraUpdateFactory.newLatLng(arg0.getPosition()));
+                            double newLong = arg0.getPosition().longitude;
+                            double newLat = arg0.getPosition().latitude;
+                            findLocation(newLat,newLong);
+                        }
+
+                        @Override
+                        public void onMarkerDrag(Marker arg0) {}
+                    });
+
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(newLocation));
                 }
             } catch (IOException e) {
